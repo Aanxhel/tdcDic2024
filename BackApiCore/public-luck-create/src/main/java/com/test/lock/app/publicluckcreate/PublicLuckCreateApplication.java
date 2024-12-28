665292091be572ea83bbdd9b0fb1f56353a3dead
Aanxhel <ang.core92@yahoo.com>
2024-12-28 00:29:48 -0600

    se agrega proyecto base, haciendo falta base de datos con api de java 8

---
+++ BackApiCore/public-luck-create/src/main/java/com/test/lock/app/publicluckcreate/PublicLuckCreateApplication.java
@@ -0,0 +1,13 @@
+package com.test.lock.app.publicluckcreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicLuckCreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicLuckCreateApplication.class, args);
+	}
+
+}
